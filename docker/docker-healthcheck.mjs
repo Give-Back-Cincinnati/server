5f2d7f9cba1a1246e3d946af94ada3493e5452cb
Michael Young <michael25young25@gmail.com>
2022-08-16 13:25:42 -0400

    updating healthcheck to log errors and reducing resource limits and requests

--- docker/docker-healthcheck.mjs
+++ docker/docker-healthcheck.mjs
@@ -9,3 +9,7 @@ axios.get('http://localhost:3000/ping')
     .then(({ status }) => {
         process.exit(status === 200 ? 0 : 1)
     })
+    .catch(e => {
+        console.error(e)
+        process.exit(1)
+    })
