3b4598b5c81129d5459b3ba800fe0db58f2ab5d2
Michael Young <michael25young25@gmail.com>
2022-08-15 22:01:58 -0400

    update to redeploy

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$NODE_ENV" = "production" ]; then
     echo "source"
     source /vault/secrets/env
     # echo "yarn permissions:seed"
-    # yarn permissions:seed
+    # yarn permissions:seed 
     echo "yarn ts-node --transpile-only src/app.ts"
     exec yarn ts-node --transpile-only src/app.ts
     #  exec yarn ts-node src/app.ts
