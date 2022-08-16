6b1c53d68d1dcbd99b379a1897d452bab7a801e8
Michael Young <michael25young25@gmail.com>
2022-08-16 09:53:34 -0400

    trying healthcheck with yarn

--- docker/docker-healthcheck.mjs
+++ docker/docker-healthcheck.mjs
@@ -3,11 +3,11 @@
 
 // we can add other thing to the healthcheck if we need to in this document
 
-process.exit(0)
+// process.exit(0)
 
-// import axios from 'axios'
+import axios from 'axios'
 
-// axios.get('http://localhost:3000/ping')
-//     .then(({ status }) => {
-//         process.exit(status === 200 ? 0 : 1)
-//     })
+axios.get('http://localhost:3000/ping')
+    .then(({ status }) => {
+        process.exit(status === 200 ? 0 : 1)
+    })
