685642a0b140aa91f188930e9bbc59ff957af2a7
Michael Young <michael25young25@gmail.com>
2022-08-16 14:55:41 -0400

    simplification

--- docker/docker-healthcheck.mjs
+++ docker/docker-healthcheck.mjs
@@ -1,16 +1,13 @@
 #!/bin/sh
-':' //# https://medium.com/@patrickleet ; exec /usr/bin/env yarn node --experimental-modules "$0" "$@"
 
-// we can add other thing to the healthcheck if we need to in this document
+import axios from 'axios'
 
-process.exit(0)
-
-// import axios from 'axios'
-
-// axios.get('http://localhost:300a/ping')
-//     .then(({ status }) => {
-//         process.exit(status === 200 ? 0 : 1)
-//     })
-//     .catch(e => {
-//         process.exit(1)
-//     })
+axios.get('http://localhost:3000/ping')
+    .then(({ status }) => {
+        console.log(status)
+        process.exit(status === 200 ? 0 : 1)
+    })
+    .catch(e => {
+        console.log(e)
+        process.exit(1)
+    })
