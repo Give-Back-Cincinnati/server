fc135ac7278b0890791104b6f622e20f858f80fe
Michael Young <michael25young25@gmail.com>
2022-08-16 14:30:16 -0400

    adding yarn in front of node

--- docker/docker-healthcheck.mjs
+++ docker/docker-healthcheck.mjs
@@ -1,13 +1,11 @@
 #!/bin/sh
-':' //# https://medium.com/@patrickleet ; exec yarn healthcheck
+':' //# https://medium.com/@patrickleet ; exec /usr/bin/env node --experimental-modules "$0" "$@"
 
 // we can add other thing to the healthcheck if we need to in this document
 
-// process.exit(0)
-
 import axios from 'axios'
 
-axios.get('http://localhost:3000/ping')
+axios.get('http://localhost:300a/ping')
     .then(({ status }) => {
         process.exit(status === 200 ? 0 : 1)
     })
