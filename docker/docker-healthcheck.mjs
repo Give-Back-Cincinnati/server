19b4a463ac028129e7802a1e1f5c85a800863f0e
Michael Young <michael25young25@gmail.com>
2022-08-16 14:59:04 -0400

    fail

--- docker/docker-healthcheck.mjs
+++ docker/docker-healthcheck.mjs
@@ -4,10 +4,8 @@ import axios from 'axios'
 
 axios.get('http://localhost:3000/ping')
     .then(({ status }) => {
-        console.log(status)
         process.exit(status === 200 ? 0 : 1)
     })
     .catch(e => {
-        console.log(e)
         process.exit(1)
     })
