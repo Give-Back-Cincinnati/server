30bc5d7f6ed1ddea7cb0892ef9ba8c265722dfdc
Michael Young <michael25young25@gmail.com>
2022-08-16 13:59:38 -0400

    more healthcheck

--- docker/docker-healthcheck.mjs
+++ docker/docker-healthcheck.mjs
@@ -7,9 +7,10 @@ import axios from 'axios'
 
 axios.get('http://localhost:3000/ping')
     .then(({ status }) => {
+        console.log(status)
         process.exit(status === 200 ? 0 : 1)
     })
     .catch(e => {
-        console.error(e)
+        console.log(e)
         process.exit(1)
     })
