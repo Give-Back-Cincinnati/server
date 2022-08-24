f7192686e22001c4e7ee58bd385a8bef014a43f8
Michael Young <michael25young25@gmail.com>
2022-08-24 12:57:50 -0400

    add set user role script to allow creation of the first superadmin

---
+++ utils/setUserRole.ts
@@ -0,0 +1,23 @@
+import { establishMongooseConnection } from "../src/mongodb"
+import { Roles } from "../src/entities/Roles"
+import { Users } from '../src/entities/Users'
+
+
+// example usage: `yarn user:setRole technology@givebackcincinnati.org SUPERADMIN`
+async function upgradeUser (email: string, role: string) {
+    try {
+        await establishMongooseConnection()
+        const foundRole = await Roles.findOne({ name: role.toUpperCase() })
+        if (!foundRole) throw new Error(`${role} not found`)
+    
+        await Users.updateOne({ email: email.toLowerCase() }, { role: foundRole })
+    } catch (e: any) {
+        console.error(e?.message)
+        process.exit(1)
+    }
+}
+
+const email = process.argv[2]
+const role = process.argv[3]
+
+upgradeUser(email, role)
